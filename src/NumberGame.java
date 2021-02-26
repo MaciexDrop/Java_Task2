85d04a6498250bdfbfb51f5ecb386a55a2918235
Maciex <macwla1994@gmail.com>
2021-02-26 23:15:04 +0100

    Added 1 to 100 random number generator.

---
+++ src/NumberGame.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NumberGame {
+
+    public static void main(String [] args){
+
+        System.out.println("Wybrałem jedną liczbę z zakresu 1-100. Masz 10 prób, potem zostaniesz zlikwidowany :)");
+        int randomNumber = (int) (Math.random()*100) + 1;
+        System.out.println(randomNumber);
+
+
+        }
+    }
+
+
