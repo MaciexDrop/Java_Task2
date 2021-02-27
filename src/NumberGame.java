3427b45a9d379c674d9dc265869dffa31e3b11f2
Maciex <macwla1994@gmail.com>
2021-02-27 09:17:03 +0100

    Added hasWon variable to check if the game was won

--- src/NumberGame.java
+++ src/NumberGame.java
@@ -2,29 +2,34 @@ import java.util.Scanner;
 
 public class NumberGame {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
 
         System.out.println("Wybrałem jedną liczbę z zakresu 1-100. Masz 10 prób aby ją zgadnąć, potem zostaniesz zlikwidowany :)");
-        int randomNumber = (int) (Math.random()*100) + 1;
-        System.out.println("Wybrana liczba to: "+randomNumber);
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        boolean hasWon = false;
+
+        System.out.println("Wybrana liczba to: " + randomNumber);
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int i=10; i>0;i--){
-            System.out.println("Wybierz liczbę. Pozostało Ci: "+i+" prób");
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Wybierz liczbę. Pozostało Ci: " + i + " prób");
             int guess = scanner.nextInt();
-            if (guess>randomNumber){
+            if (guess > randomNumber) {
                 System.out.println("Za dużo, wybierz mniej");
-                } else if (guess<randomNumber){
+            } else if (guess < randomNumber) {
                 System.out.println("Za mało, wybierz więcej");
-                } else {
-                System.out.println("Brawo! Wygrywasz!");
+            } else {
+                hasWon = true;
                 break;
             }
         }
-        System.out.println("Przegrywasz!");
-
+        if (hasWon) {
+            System.out.println("Brawo! Wygrywasz!");
+        } else {
+            System.out.println("Przegrywasz!");
+            System.out.println("Wybranym numerem było: " + randomNumber);
         }
     }
-
+}
 
